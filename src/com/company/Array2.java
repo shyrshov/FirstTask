acdb4de739155ae454dd9de9824261b64e2782df
Andrii <a.shyrshov@gmail.com>
2021-03-15 10:41:12 +0200

    Array2 changes to last element

--- src/com/company/Array2.java
+++ src/com/company/Array2.java
@@ -2,7 +2,7 @@ public class Array2 {
     public static void main(String[] args){
         double[] array2 = {12.12, 1.666, 44.01, 1.1, 0.443};
         double a = array2[0];
-        double b = array2[4];
+        double b = array2[array2.length - 1];
         System.out.println("First and the last elements of array are: " + a + ", " + b);
     }
 }
